2a61eeb67c47fb181ee319659837187035914c5a
Gustavo Barreto <gustavo.toniolo@hotmail.com>
2023-10-25 22:06:35 -0300

    Alteração limite -50

--- app.js
+++ app.js
@@ -54,11 +54,11 @@ function reiniciarJogo() {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do Número Secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 }
 
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 exibirMensagemInicial();
